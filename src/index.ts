4d3426436f38eb54982a0bdfdca5f33d5d2fe4e1
KF <kofishmalofi@gmail.com>
2020-02-07 12:38:35 -0500

    test setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-
 import './styles.css';
+//import { add } from '../spec/utils';  since added own math fie
+import { add } from './math';
 console.log('Ready to Party With Some TypeScript!');
 
+console.log(add(3, 5));
